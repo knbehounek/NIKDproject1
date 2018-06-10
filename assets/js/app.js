3b0602d164a7aa193eb4953cb05e6480c190a1f0
Kelby Behounek <knbehounek@hotmail.com>
2018-06-10 15:22:32 -0700

    html v1

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyBbukVkHU7qdQYVs-Raj2DlPA9t9s1YlO4",
+    authDomain: "nikd-67dbe.firebaseapp.com",
+    databaseURL: "https://nikd-67dbe.firebaseio.com",
+    projectId: "nikd-67dbe",
+    storageBucket: "nikd-67dbe.appspot.com",
+    messagingSenderId: "1041995690239"
+  };
+  firebase.initializeApp(config);
