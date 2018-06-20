15807d85a3dfeddc25c7ae43a9e76ae6a6dfa0c3
NathanNovak <nnovak@ku.edu>
2018-06-19 21:03:59 -0700

    added button for city search

--- assets/js/front movie box.js
+++ assets/js/front movie box.js
@@ -4,6 +4,9 @@ var queryURLMovies = "https://api.themoviedb.org/3/movie/now_playing?api_key=1c5
 var queryURLConcerts = "https://app.ticketmaster.com/discovery/v2/events.json?size=20&city=tucson&apikey=b8OAoL9Cio2JXSXZxlJ0ZAeGr2k0iwVL"
 
 $(document).ready(function () {
+  movieCall();
+  concertCall();
+  
   function removeDups(arrOfObj) {
     arrOfObj.sort(function (a, b) {
       var textA = a.name.toUpperCase();
@@ -25,40 +28,123 @@ $(document).ready(function () {
 
   //Appends Movie Posters with click events to index
 
-  $.ajax({
-    url: queryURLMovies,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
+  $('#concertSearch').on('click', function(event){
+    event.preventDefault();
+    var cityInput = $('#cityInput').val();
+    $.ajax({
+      type: "GET",
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?size=20&city="+cityInput+"&apikey=b8OAoL9Cio2JXSXZxlJ0ZAeGr2k0iwVL",
+      async: true,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+        $('.concertResults').empty();
+  
+        // Do other things.
+        var concertInfo = response._embedded.events;
+        console.log("CONCERT", concertInfo);
+        var filteredConcertInfo = removeDups(concertInfo);
+        console.log("FILTERED INFO", filteredConcertInfo);
+  
+        for (var i = 0; i < filteredConcertInfo.length; i++) {
+  
+  
+          var concertDiv = $('<div></div>');
+          concertDiv.addClass('eachConcert');
+          // var unionNames = _.union(concertInfo[i].name);
+          // console.log(concertInfo[i].name);
+  
+          var concertName = filteredConcertInfo[i].name;
+          var concertDate = filteredConcertInfo[i].dates.start.localDate;
+          console.log(concertDate);
+          var dateForConcert = $('<p>').text(concertDate);
+          var titleForConcert = $('<p>').text(concertName);
+          console.log("Concert Name", concertName);
+  
+  
+  
+          $('.concertResults').append(concertDiv);
+          var imgDiv = $('<div></div>');
+          var concertImage = $('<img>');
+          var urlTarget = "";
+          var imageLink = $('<a>').attr('href', filteredConcertInfo[i].url);
+          imageLink.attr('target', "_blank");
+  
+  
+          concertImage.addClass('concertDisplayPosters');
+          concertImage.attr('concertId', filteredConcertInfo[i]);
+          console.log("ATTRACTIONS", filteredConcertInfo[i]);
+          imageLink.html(concertImage);
+  
+          imgDiv.append(imageLink);
+          concertDiv.append(titleForConcert);
+          concertDiv.append(dateForConcert);
+  
+          for (var j = 0; j < filteredConcertInfo[i].images.length; j++) {
+            var picWidth = filteredConcertInfo[i].images[j].width;
+            var picHeight = filteredConcertInfo[i].images[j].height;
+  
+            if (picWidth === 205 && picHeight === 115) {
+              console.log("concert Images", filteredConcertInfo[i].images[j].url);
+              concertImage.attr({ src: filteredConcertInfo[i].images[j].url });
+              concertDiv.append(imageLink);
+            }
+          }
+  
+          // concertImage.attr({src: filteredConcertInfo[i].images[j].ratio});
+          // console.log('j',j)
+          // var concertImage = $(`<div style="height: 200px; width: 175px;"><img src="${filteredConcertInfo[i].images[0].url}" style="width: 100%;"></div>`);
+  
+          // }
+          $('.concertResults').append(concertDiv);
+        }
+        // error: function(xhr, status, err) {
+        //             // This time, we do not end up here!
+        //          }
 
-    var movieInfo = response.results;
-    console.log(movieInfo);
+      }})
+      
 
-    for (var i = 0; i < movieInfo.length; i++) {
-
-      var movieDiv = $('<div></div>');
-      movieDiv.addClass('eachMovie');
-
-      var voteAverage = movieInfo[i].vote_average;
-      var titleForMovie = $('<p>').text('Vote_Average: ' + voteAverage);
-      console.log(voteAverage);
-
-
-      $('movieResults').append(movieDiv);
-
-      var movieImage = $('<img>');
-     
-      movieImage.addClass('displayPosters');
-      movieImage.attr({ src: "http://image.tmdb.org/t/p/w185/" + movieInfo[i].poster_path });
-      movieImage.attr("movieId", movieInfo[i].id);
-      console.log("Movie Id" + movieInfo[i].id);
-     
-      movieDiv.append(movieImage);
-      movieDiv.append(titleForMovie);
-
-      $('.movieResults').append(movieDiv);
-    }
+    console.log(cityInput);
   })
+
+  function movieCall(){
+    $.ajax({
+      url: queryURLMovies,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+  
+      var movieInfo = response.results;
+      console.log(movieInfo);
+  
+      for (var i = 0; i < movieInfo.length; i++) {
+  
+        var movieDiv = $('<div></div>');
+        movieDiv.addClass('eachMovie');
+  
+        var voteAverage = movieInfo[i].vote_average;
+        var titleForMovie = $('<p>').text('Peer Rating ot of 10: ' + voteAverage);
+        console.log(voteAverage);
+  
+  
+        $('movieResults').append(movieDiv);
+  
+        var movieImage = $('<img>');
+       
+        movieImage.addClass('displayPosters');
+        movieImage.attr({ src: "http://image.tmdb.org/t/p/w185/" + movieInfo[i].poster_path });
+        movieImage.attr("movieId", movieInfo[i].id);
+        console.log("Movie Id" + movieInfo[i].id);
+       
+        movieDiv.append(movieImage);
+        movieDiv.append(titleForMovie);
+  
+        $('.movieResults').append(movieDiv);
+      }
+    })
+  }
+  
 
   $(this).on('click', '.displayPosters', function () {
 
@@ -80,6 +166,80 @@ $(document).ready(function () {
   })
 
   //Appends the Concerts to the Index html
+  function concertCall(){
+    $.ajax({
+      type: "GET",
+      url: queryURLConcerts,
+      async: true,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+        // Parse the response.
+  
+        // Do other things.
+        var concertInfo = response._embedded.events;
+        console.log("CONCERT", concertInfo);
+        var filteredConcertInfo = removeDups(concertInfo);
+        console.log("FILTERED INFO", filteredConcertInfo);
+  
+        for (var i = 0; i < filteredConcertInfo.length; i++) {
+  
+  
+          var concertDiv = $('<div></div>');
+          concertDiv.addClass('eachConcert');
+          // var unionNames = _.union(concertInfo[i].name);
+          // console.log(concertInfo[i].name);
+  
+          var concertName = filteredConcertInfo[i].name;
+          var concertDate = filteredConcertInfo[i].dates.start.localDate;
+          console.log(concertDate);
+          var dateForConcert = $('<p>').text(concertDate);
+          var titleForConcert = $('<p>').text(concertName);
+          console.log("Concert Name", concertName);
+  
+  
+  
+          $('.concertResults').append(concertDiv);
+          var imgDiv = $('<div></div>');
+          var concertImage = $('<img>');
+          var urlTarget = "";
+          var imageLink = $('<a>').attr('href', filteredConcertInfo[i].url);
+          imageLink.attr('target', "_blank");
+  
+  
+          concertImage.addClass('concertDisplayPosters');
+          concertImage.attr('concertId', filteredConcertInfo[i]);
+          console.log("ATTRACTIONS", filteredConcertInfo[i]);
+          imageLink.html(concertImage);
+  
+          imgDiv.append(imageLink);
+          concertDiv.append(titleForConcert);
+          concertDiv.append(dateForConcert);
+  
+          for (var j = 0; j < filteredConcertInfo[i].images.length; j++) {
+            var picWidth = filteredConcertInfo[i].images[j].width;
+            var picHeight = filteredConcertInfo[i].images[j].height;
+  
+            if (picWidth === 205 && picHeight === 115) {
+              console.log("concert Images", filteredConcertInfo[i].images[j].url);
+              concertImage.attr({ src: filteredConcertInfo[i].images[j].url });
+              concertDiv.append(imageLink);
+            }
+          }
+  
+          // concertImage.attr({src: filteredConcertInfo[i].images[j].ratio});
+          // console.log('j',j)
+          // var concertImage = $(`<div style="height: 200px; width: 175px;"><img src="${filteredConcertInfo[i].images[0].url}" style="width: 100%;"></div>`);
+  
+          // }
+          $('.concertResults').append(concertDiv);
+        }
+        // error: function(xhr, status, err) {
+        //             // This time, we do not end up here!
+        //          }
+      }
+    })
+  }
 
   $.ajax({
     type: "GET",
